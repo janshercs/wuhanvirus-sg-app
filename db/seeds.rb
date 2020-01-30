a059e23ff3821c1e9f29afb5f2eba5b498e20c84
zp <zhipeng34@gmail.com>
2020-01-31 01:32:01 +0800

    fix up seed to store new data format

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'uri'
+
 Article.create!(
-  link: "https://mothership.sg/2020/01/wuhan-virus-sbcc-tiong-bahru/", 
   headline: "Suspected case at Tiong Bahru baby clinic tests negative for Wuhan coronavirus",
   preview: "Suspected case at Tiong Bahru Plaza has been confirmed to be negative",
   source: "Mothership.sg"  
 )
+
+url = 'https://cna-sg-res.cloudinary.com/image/upload/q_auto,f_auto/image/12364452/16x9/991/557/ee873e0361b45d91213d8290708f23e4/vm/people-wearing-protective-face-mask-at-orchard-road--singapore-where-five-case-of-the-wuhan-coronavirus-has-been-confirmed--1-.jpg'
+
+filename = File.basename(URI.parse(url).path)
+file = URI.open(url)
+
+Article.first.image.attach(io: file, filename: filename)
+
+Shortener::ShortenedUrl.generate("https://mothership.sg/2020/01/wuhan-virus-sbcc-tiong-bahru/", owner: Article.first)
