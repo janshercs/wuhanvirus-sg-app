ee25576009be50b58dc07382df564372e27e2c0d
zp <zhipeng34@gmail.com>
2020-01-31 01:11:50 +0800

    set default time zone to singapore

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module WuhanvirusSg
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # set timezone to singapore
+    config.time_zone = "Singapore"
   end
 end
