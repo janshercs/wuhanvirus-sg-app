240692ef489fd33907839a6afa3ae6c2cf2703cb
zp <zhipeng34@gmail.com>
2020-01-31 22:05:28 +0800

    not the best way, but test on prod to make sure the js does indeed work

--- Gemfile
+++ Gemfile
@@ -27,7 +27,7 @@ gem "mini_magick"
 gem "shortener"
 gem 'ahoy_matey'
 gem 'maxminddb'
-gem 'turbolinks'
+gem 'momentjs-rails'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
-//= require rails-ujs
 //= require jquery-3.2.1.min
+//= require rails-ujs
+//= require moment
 //= require plugins/zebra-tooltip/zebra_tooltips.min
 //= require plugins/owl-carousel/owl.carousel.min.js
 //= require main-script
