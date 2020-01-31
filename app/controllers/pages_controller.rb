3b55aaed2476e9ca968e1c3e72c35cb51acd1c48
zp <zhipeng34@gmail.com>
2020-02-01 00:52:18 +0800

    further fix up the maps

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
   end
 
   def map
-    @dates = (Date.parse("21/01/2020")..Date.today).map{ 
+    @dates = (Date.parse("23/01/2020")..Date.today).map{ 
       |d| d.strftime("#{d.day.ordinalize} %B %Y") 
     }
   end
